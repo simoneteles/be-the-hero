3e8c71f9adcc04bbb180188e0ac908e2a09944db
Simone Teles <siteles2@gmail.com>
2020-05-05 00:28:12 -0300

    front-end da aplicação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './global.css';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Rélooouuuuu! It's me!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          RocketSeet
-        </a>
-      </header>
-    </div>
+   <Routes />
   );
 }
 
